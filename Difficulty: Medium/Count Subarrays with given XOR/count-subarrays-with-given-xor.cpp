ad4f98651eebe2cf806b94bad39dfcc8c811b043
abhaypatel20 <abhay.patel.cs24@ggits.net>
2026-06-02 23:30:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+     int n =arr.size();
+     int xr =0;
+     map<int,int>mpp;
+     mpp[xr]++;
+    int cnt = 0;
+     for(int i=0;i<n;i++){
+      xr = xr^ arr[i];
+     
+      int x = xr^k;
+      cnt += mpp[x];
+       mpp[xr]++;
+     }
+        return cnt;
+    }
+};
