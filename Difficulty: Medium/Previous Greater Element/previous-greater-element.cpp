652e428fa30f52ebedbcded5ea97f612724b387d
abhaypatel20 <abhay.patel.cs24@ggits.net>
2026-06-17 22:26:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Greater Element/previous-greater-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> preGreaterEle(vector<int>& arr) {
+        stack<int> st;
+        vector<int> ans;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+           while(!st.empty() && st.top()<=arr[i]){
+               st.pop();
+           }
+           if(st.empty()){
+               ans.push_back(-1);
+           }else{
+               ans.push_back(st.top());
+           }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
+        
+  
